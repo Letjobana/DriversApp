febd7e98f117db977cdc637ded01900fd9b9c607
Letjobana <suzanm@gerbertech.co.za>
2022-05-22 21:05:42 +0200

    Added a SectionsController to get sections from the database

---
+++ DriversApp/DriversApp/Controllers/SectionsController.cs
@@ -0,0 +1,43 @@
+﻿using DriversApp.Data;
+using DriversApp.Repositories.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriversApp.Controllers
+{
+    public class SectionsController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public SectionsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult GetSections()
+        {
+            try
+            {
+                return Ok(unitOfWork.SectionRepository.GetAllSections());
+
+            }
+            catch (Exception ex)
+            {
+
+                var errorObjectResult = new ObjectResult(new
+                {
+                    Message = "An error occurred while retrieving data from the database. " +
+                       "Please try again, and if the problem persists, contact support@gmail.com",
+                    Status = "Error",
+                    Exception = ex.Message
+                });
+                errorObjectResult.StatusCode = StatusCodes.Status500InternalServerError;
+                return errorObjectResult;
+            }
+        }
+    }
+}
