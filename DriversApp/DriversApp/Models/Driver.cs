019c5df440f0c7389991efffba412385588c1d0b
Letjobana <suzanm@gerbertech.co.za>
2022-05-22 19:32:20 +0200

    Added a class called Driver

---
+++ DriversApp/DriversApp/Models/Driver.cs
@@ -0,0 +1,12 @@
+﻿namespace DriversApp.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Section { get; set; }
+    }
+}
