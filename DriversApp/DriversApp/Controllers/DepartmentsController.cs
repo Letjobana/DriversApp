0fdcedb7dfafc4cdb1aa29c0192f0dcd0d23fa0c
Letjobana <suzanm@gerbertech.co.za>
2022-05-22 20:57:54 +0200

    Added a controller Department to  get departments from the database

---
+++ DriversApp/DriversApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,41 @@
+﻿using DriversApp.Repositories.Abstracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriversApp.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DepartmentsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public IActionResult GetDepartments()
+        {
+            try
+            {
+                return Ok(unitOfWork.DepartmentRepository.GetAllDepartments());
+            }
+            catch (Exception ex)
+            {
+
+                var errorObjectResult = new ObjectResult(new
+                {
+                    Message = "An error occurred while retrieving data from the database. " +
+                       "Please try again, and if the problem persists, contact support@gmail.com",
+                    Status = "Error",
+                    Exception = ex.Message
+                });
+                errorObjectResult.StatusCode = StatusCodes.Status500InternalServerError;
+                return errorObjectResult;
+            }
+        }
+    }
+}
