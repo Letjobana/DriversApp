cd72644c969fe545023f320e7cf9b9c8746a89d1
Letjobana <suzanm@gerbertech.co.za>
2022-05-22 19:46:16 +0200

    Added a class called ApplicationDbContext for database connection

---
+++ DriversApp/DriversApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DriversApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriversApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>option):base(option)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Requirement> Requirements { get; set; }
+        public DbSet<Section> Sections { get; set; }
+    }
+}
