2ed3d7b74fdda57d1936e85989c2defd18ab1713
Letjobana <suzanm@gerbertech.co.za>
2022-05-22 20:17:03 +0200

    Added a class called DriverViewModel

---
+++ DriversApp/DriversApp/ViewModels/DriverViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DriversApp.ViewModels
+{
+    public class DriverViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Section { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
